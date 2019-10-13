f063289ba17030161f7bb6b162cba6045edc359d
Emily <lalonde0902@gmail.com>
2019-10-13 15:41:37 -0600

    Set up Nav component and Nav test

---
+++ src/Nav/Nav.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Nav from './Nav';
+
+describe('Nav', () => {
+  it('should return true', () => {
+    expect(true).toEqual(true)
+  })
+})
