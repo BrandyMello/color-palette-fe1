1113be79587e67f13db9a52ca0dd9276ed43a48f
Brandy Mello <brandyamello@gmail.com>
2019-10-14 16:02:59 -0600

    Add input and submit button for new palette in save form

--- src/SaveForm/SaveForm.js
+++ src/SaveForm/SaveForm.js
@@ -4,10 +4,12 @@ const SaveForm = ({ projects }) => {
   console.log(projects)
   let eachProject = projects.map((project, index) => <option key={index} value={project.name}>{ project.name }</option>)
   return (
-    <form>
+    <form name="newPalette">
       <select>
         { eachProject }
       </select>
+      <input name="newPalette" placeholder="New Palette Name"></input>
+      <button name="newPalette" type="submit">Submit</button>
     </form>
     
   )
