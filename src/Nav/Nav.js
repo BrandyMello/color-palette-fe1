01107d73e5b0af1248e249c6b4fd74830375089e
Brandy Mello <brandyamello@gmail.com>
2019-10-15 00:30:43 -0600

    Add some minor styles mainly spa=cing project palettes

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -7,9 +7,10 @@ class Nav extends Component {
   }
   render() {
     return (
-      <div>
-        <h1>Color Picker</h1>
-        <p>insert logo</p>
+      <div className="navBar">
+        <h1>Color Palette</h1>
+        {/* <img src="https://user-images.githubusercontent.com/46384968/66802881-2ce58a80-eedb-11e9-97f2-e60bf5a8e28b.png" className='logo'></img> */}
+        {/* <p>insert logo</p> */}
         <form>
           <input placeholder= "search"/>
           <button type="submit">Submit</button>
