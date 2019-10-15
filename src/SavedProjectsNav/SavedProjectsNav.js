19c199d248fd4f6b19dc874b90a48e540f7bf717
Brandy Mello <brandyamello@gmail.com>
2019-10-14 22:47:14 -0600

    Add route for home to fix double routes showing up unsuccessful at linking on click so Add display for project palettes

--- src/SavedProjectsNav/SavedProjectsNav.js
+++ src/SavedProjectsNav/SavedProjectsNav.js
@@ -1,7 +1,13 @@
 import React from 'react';
+// import { NavLink } from 'react-router-dom';
+
+// const goToProject = () => {
+
+// }
 
 const SavedProjectsNav = ({ projects }) => {
 let project = projects.map((project, index) => <button key={index}>{project.name}</button>);
+console.log(project.id)
   return (
     <div>
       { project }
