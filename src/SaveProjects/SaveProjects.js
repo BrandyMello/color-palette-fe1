50e4124c7a38dd453257ac3a5475d4f89a5271ba
Emily <lalonde0902@gmail.com>
2019-10-15 16:16:43 -0600

    Create function to save a new project name that has been entered by a user

---
+++ src/SaveProjects/SaveProjects.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import './SaveProjects.css'
+import {addNewProject} from '../apiCalls/apiCalls'
+
+class SaveProjects extends Component {
+  constructor() {
+    super()
+    this.state = {
+      projectName: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  saveNewProject = async(e) => {
+    e.preventDefault()
+    console.log('this.state.projectName', this.state.projectName)
+    addNewProject({name: this.state.projectName})
+  }
+  
+  render() {
+    return (
+    <form>
+      <input 
+      type="text"
+      id="name"
+      name="projectName"
+      placeholder="New Project Name"
+      value={this.state.projectName} 
+      onChange={this.handleChange}
+      />
+      <button 
+      name="newProject"
+      type="submit"
+      onClick={ this.saveNewProject}
+      >
+      Create New Project
+      </button>
+    </form>
+    )
+  }
+}
+
+export default SaveProjects
