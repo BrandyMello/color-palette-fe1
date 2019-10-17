129de4dc3da5fd69c81285e53764f19ebea80896
Emily <lalonde0902@gmail.com>
2019-10-16 19:39:50 -0600

    Add tests to RandomPalette

--- src/Nav/Nav.test.js
+++ src/Nav/Nav.test.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Nav from './Nav';
+import React from "react";
+import { shallow } from "enzyme";
+import Nav from "./Nav";
 
-describe('Nav', () => {
-  it('renders the title of the app in <h1> tags', () => {
+describe("Nav", () => {
+  it("renders the title of the app in <h1> tags", () => {
     const wrapper = shallow(<Nav title="Bananas" />);
     const title = <h1>Color Palette</h1>;
 
     expect(wrapper.contains(title)).toEqual(true);
   });
-  it('renders the image of the logo for the app in an <img> tag', () => {
-    const wrapper = shallow(<Nav img="https://user-images.githubusercontent.com/46384968/66802881-2ce58a80-eedb-11e9-97f2-e60bf5a8e28b.png" className='logo' />);
-    const img = <img src="https://user-images.githubusercontent.com/46384968/66802881-2ce58a80-eedb-11e9-97f2-e60bf5a8e28b.png" className='logo'></img>;
+  it("renders the image of the logo for the app in an <img> tag", () => {
+    const wrapper = shallow(
+      <Nav
+        img="https://user-images.githubusercontent.com/46384968/66802881-2ce58a80-eedb-11e9-97f2-e60bf5a8e28b.png"
+        className="logo"
+      />
+    );
+    const img = (
+      <img
+        src="https://user-images.githubusercontent.com/46384968/66802881-2ce58a80-eedb-11e9-97f2-e60bf5a8e28b.png"
+        className="logo"
+      ></img>
+    );
 
     expect(wrapper.contains(img)).toEqual(true);
   });
-  it('should match the snapshot', () => {
-    const wrapper = shallow(<Nav title='Color Palette' img="https://user-images.githubusercontent.com/46384968/66802881-2ce58a80-eedb-11e9-97f2-e60bf5a8e28b.png" className='logo' />)
+  it("should match the snapshot", () => {
+    const wrapper = shallow(
+      <Nav
+        title="Color Palette"
+        img="https://user-images.githubusercontent.com/46384968/66802881-2ce58a80-eedb-11e9-97f2-e60bf5a8e28b.png"
+        className="logo"
+      />
+    );
 
     expect(wrapper).toMatchSnapshot();
   });
-})
+});
--- src/SaveProjects/SaveProjects.test.js
+++ src/SaveProjects/SaveProjects.test.js
@@ -1,51 +1,54 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import SaveProjects from './SaveProjects';
+import React from "react";
+import { shallow } from "enzyme";
+import SaveProjects from "./SaveProjects";
 
-describe('SaveProjects', () => {
+describe("SaveProjects", () => {
   let wrapper;
   let mockProjects;
 
   beforeEach(() => {
-mockProjects = [
-  {name:'Autumn', id: 1},
-  {name:'Seabreeze', id: 2}
-]
+    mockProjects = [{ name: "Autumn", id: 1 }, { name: "Seabreeze", id: 2 }];
 
-    wrapper = shallow(<SaveProjects projects={mockProjects} saveNewProject={jest.fn()} />);
+    wrapper = shallow(
+      <SaveProjects projects={mockProjects} saveNewProject={jest.fn()} />
+    );
   });
 
-  it('should match snapshot', () => {
+  it("should match snapshot", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should update state when handleChange is called', () => {
-    const mockEvent = { target: { name: 'projectName', value: {name: 'Fall', id: 3}} };
-    const expected = {name: 'Fall', id: 3};
+  it("should update state when handleChange is called", () => {
+    const mockEvent = {
+      target: { name: "projectName", value: { name: "Fall", id: 3 } }
+    };
+    const expected = { name: "Fall", id: 3 };
 
     wrapper.instance().handleChange(mockEvent);
 
-    expect(wrapper.state('projectName')).toEqual(expected);
+    expect(wrapper.state("projectName")).toEqual(expected);
   });
 
-  it('should reset state when clearInput is called', () => {
-    const expected = {projectName: ''};
+  it("should reset state when clearInput is called", () => {
+    const expected = { projectName: "" };
 
-    wrapper.instance().setState({projectName: 'Fall'});
+    wrapper.instance().setState({ projectName: "Fall" });
 
     wrapper.instance().clearInput();
 
     expect(wrapper.state()).toEqual(expected);
   });
 
-  it('should run handleChange when the inputs detect a change', () => {
-    const mockEvent = { target: { name: 'projectName', value: {name: 'Fall', id: 3}} }
+  it("should run handleChange when the inputs detect a change", () => {
+    const mockEvent = {
+      target: { name: "projectName", value: { name: "Fall", id: 3 } }
+    };
 
     wrapper.instance().handleChange = jest.fn();
-    wrapper.instance().forceUpdate()
-    wrapper.update()
-    wrapper.find('[name="projectName"]').simulate('change', mockEvent);
+    wrapper.instance().forceUpdate();
+    wrapper.update();
+    wrapper.find('[name="projectName"]').simulate("change", mockEvent);
 
     expect(wrapper.instance().handleChange).toHaveBeenCalledWith(mockEvent);
   });
-})
+});
