a3f8f5856200c692dec29e8461af4f5dcefe1b87
Brandy Mello <brandyamello@gmail.com>
2019-10-16 22:23:58 -0600

    Add snapshot to App test and alter ProjectsDropDown test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
 });
