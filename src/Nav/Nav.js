0b6d8c7b4360bb5e0775374e909de2a333d475e4
Emily <lalonde0902@gmail.com>
2019-10-16 22:28:46 -0600

    Upload completed tests so far

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -6,7 +6,7 @@ const Nav = () => {
     return (
       <div className="navBar">
         <h1>Color Palette</h1>
-        <img src="https://user-images.githubusercontent.com/46384968/66802881-2ce58a80-eedb-11e9-97f2-e60bf5a8e28b.png" className='logo'></img>
+        <img src="https://user-images.githubusercontent.com/46384968/66802881-2ce58a80-eedb-11e9-97f2-e60bf5a8e28b.png" className='logo' alt="Frida"></img>
       </div>
     )
 }
