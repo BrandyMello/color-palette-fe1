d715f69cd5e588020391925e20ddb59df8a0053c
Brandy Mello <brandyamello@gmail.com>
2019-10-16 23:53:32 -0600

    Add two set state tests and on delete from palettes which is not working

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,78 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Projects from './Projects/Projects';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import { toBindingIdentifierName } from '@babel/types';
 
 describe('App', () => {
   it('should match the snapshot', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update the projects array in state when when a new project is saved is called', () => {
+    const mockProjects = [{
+      id: 1,
+      name: 'Warm Colors'
+    }];
+    const expectedProjects = [{
+      id: 1,
+      name: 'Warm Colors'
+    }];
+
+    const wrapper = shallow(<App />)
+    
+    wrapper.setState({ projects: mockProjects });
+    expect(wrapper.state('projects')).toEqual(expectedProjects);
+  });
+
+  it('should update palettes in state when a new palette is added is called', () => {
+    const mockPalettes = [
+      {
+        colorFive: "#472C25",
+        colorFour: "#91371B",
+        colorOne: "#F7E5D4",
+        colorThree: "#EB712F",
+        colorTwo: "#FADDAF",
+        name: "Spiced",
+        projectId: 1
+      }
+    ]
+    const mockNewPalettes = [
+      {
+        colorFive: "#472C25",
+        colorFour: "#91371B",
+        colorOne: "#F7E5D4",
+        colorThree: "#EB712F",
+        colorTwo: "#FADDAF",
+        name: "Spiced",
+        projectId: 1
+      }
+    ]
+
+    const wrapper = shallow(<App />)
+    wrapper.setState({ palettes: mockPalettes });
+    expect(wrapper.state('palettes')).toEqual(mockNewPalettes);
+  });
+
+  it('should delete a specific palette when deleteSpecificPalette is called', () => {
+    const originalPalettes = [
+      {
+        colorFive: "#472C25",
+        colorFour: "#91371B",
+        colorOne: "#F7E5D4",
+        colorThree: "#EB712F",
+        colorTwo: "#FADDAF",
+        name: "Spiced",
+        projectId: 1
+      }
+    ]
+
+    const wrapper = shallow(<App />)
+    wrapper.setState({palettes: originalPalettes});
+    wrapper.instance().deleteSpecificPalette(1);
+    expect(wrapper.state('palettes')).toEqual([])
   });
 });
 
